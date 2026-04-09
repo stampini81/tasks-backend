1ecaa7de118449d353287194e9cb269eda5d8257
Leandro da Silva Stampini <leandro.stampini@aircompany.ai>
2026-04-09 15:26:22 -0300

    Hide DateUtils constructor

--- src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
+++ src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 
 public class DateUtils {
 
+	private DateUtils() {
+	}
+
 	public static boolean isEqualOrFutureDate(LocalDate date) {
 		return date.isEqual(LocalDate.now()) || date.isAfter(LocalDate.now());
 	}
